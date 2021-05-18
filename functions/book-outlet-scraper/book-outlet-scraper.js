a243f1f5a2fa6e8f355658a76d4ec9202c007972
Tristan Hampton <tristanhampton@icloud.cmo>
2021-05-17 21:28:04 -0600

    foo

--- functions/book-outlet-scraper/book-outlet-scraper.js
+++ functions/book-outlet-scraper/book-outlet-scraper.js
@@ -2,7 +2,7 @@ const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event, context) => {
 
-	url = 'https://bookoutlet.ca/Store/Browse?Nc=31&Nw=0&size=24&sort=popularity_0&Ns=600'
+	const url = 'https://bookoutlet.ca/Store/Browse?Nc=31&Nw=0&size=24&sort=popularity_0&Ns=600';
 
 
 	const browser = await chromium.puppeteer.launch({
