a3d6557d9e0e43acd779a9e90c952bb0ceb02b11
Tristan Hampton <tristanhampton@icloud.cmo>
2021-05-22 11:00:16 -0600

    trying to scrape

--- functions/book-outlet-scraper/book-outlet-scraper.js
+++ functions/book-outlet-scraper/book-outlet-scraper.js
@@ -2,33 +2,35 @@ const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event, context) => {
 
-	const url = 'https://bookoutlet.ca/Store/Browse?Nc=31&Nw=0&size=24&sort=popularity_0&Ns=600';
+	const url = 'https://bookoutlet.ca/Store/Browse?Nc=31';
 
 
-	// const browser = await chromium.puppeteer.launch({
- //        executablePath: await chromium.executablePath,
- //        args: chromium.args,
- //        defaultViewport: chromium.defaultViewport,
- //        headless: chromium.headless,
- //    });
+	const browser = await chromium.puppeteer.launch({
+        executablePath: await chromium.executablePath,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless,
+    });
 
-    // const page = await browser.newPage();
+    const page = await browser.newPage();
 
-    // await page.goto(url);
-    // await page.waitForSelector('a.line-clamp-2');
+    await page.goto(url);
+    await page.waitForSelector('a.line-clamp-2');
 
-    // booktitle = await page.evaluate(() => {
-    // 	const title = document.querySelector('a.line-clamp-2').text;
-    // 	return title;
-    // });
+    const results = await page.evaluate(() => {
+    	let titleNodeList = document.querySelectorAll('a.line-clamp-2');
+        const titleList = [];
+        titleNodeList.forEach(title => titleList.push(title.dataset.text));
+    	return titleList;
+    });
 
-    // await browser.close();
+    await browser.close();
 
     return {
     	statusCode: 200,
     	body: JSON.stringify({
     		message: 'Completed Scraping',
-    		content: booktitle
+    		content: titleList,
     	})
     }
 }
