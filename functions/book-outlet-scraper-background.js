becb1d311ef63fad0072c9ab69fd3a8148f253b7
Tristan Hampton <tristanhampton@icloud.cmo>
2021-05-22 12:04:15 -0600

    Trying to find bad token

--- functions/book-outlet-scraper-background.js
+++ functions/book-outlet-scraper-background.js
@@ -1,50 +1,50 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context) => {
-    let browser = null;
-    let theTitle = null;
-	const url = 'https://bookoutlet.ca/Store/Browse?Nc=31';
+    let browser = null
+    let theTitle = null
+	const url = 'https://bookoutlet.ca/Store/Browse?Nc=31'
 
-    console.log('spawning chrome headless');
+    console.log('spawning chrome headless')
     try {
-        const executablePath = await chromium.executablePath;
+        const executablePath = await chromium.executablePath
 
         // setup
         browser = await puppeteer.launch({
           args: chromium.args,
           executablePath: executablePath,
           headless: chromium.headless,
-        });
+        })
 
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
-        await page.waitForSelector('a.line-clamp-2');
+        const page = await browser.newPage()
+        await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] })
+        await page.waitForSelector('a.line-clamp-2')
 
-        theTitle = await page.title();
+        theTitle = await page.title()
 
         // const results = await page.evaluate(() => {
-        // 	let titleNodeList = document.querySelectorAll('a.line-clamp-2');
-        //     const titleList = [];
-        //     titleNodeList.forEach(title => titleList.push(title.dataset.text));
-        // 	return titleList;
-        // });
+        // 	let titleNodeList = document.querySelectorAll('a.line-clamp-2')
+        //     const titleList = []
+        //     titleNodeList.forEach(title => titleList.push(title.dataset.text))
+        // 	return titleList
+        // })
 
-        console.log('done on page', theTitle);
-        
+        console.log('done on page', theTitle)
+
     } catch (error) {
-        console.log('error', error);
+        console.log('error', error)
 
         return {
             statusCode: 500,
             body: JSON.stringify({
                 error: error
-            });
+            })
         }
     } finally {
         //--- Close browser
         if(browser !== null) {
-            await browser.close();
+            await browser.close()
         }
     }
 
